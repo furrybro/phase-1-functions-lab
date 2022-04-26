bbc95507625783287cb77ebab3de3a136e449f75
Jeena Park <jeenapark15@gmail.com>
2022-04-26 12:28:25 -0400

    commit tree 1709e7dd35c6191053b0bd0c4d7257c741b6d080

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    let hq = 42
+    return Math.abs(pickup - hq);
+}
+function distanceFromHqInFeet(totalFeet) {
+    return distanceFromHqInBlocks(totalFeet) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }  else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    }  else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    }  else if (distanceTravelledInFeet(start, destination) >= 2500) {
+        return 'cannot travel that far';
+    }
+
+}
